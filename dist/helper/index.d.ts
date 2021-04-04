93bb9919b3ab0b33f68d60a6ea75e0663a163fd6
afarukcali <afarukcali@gmail.com>
2021-04-04 21:57:25 +0300

    dist and test index.html.

--- dist/helper/index.d.ts
+++ dist/helper/index.d.ts
@@ -4,5 +4,6 @@ declare const hexString: (data: string) => string;
 declare const hexLittleEndian: (hex: string) => string;
 declare const fillStackDataByte: (byteInput: string) => StackData;
 declare const fillStackDataNumber: (input: string) => StackData;
+declare const fillStackDataString: (input: string) => StackData;
 declare const parseInput: (input: string) => StackData;
-export { fillStackDataByte, fillStackDataNumber, hexLittleEndian, hexNumber, hexString, parseInput };
+export { fillStackDataByte, fillStackDataNumber, fillStackDataString, hexLittleEndian, hexNumber, hexString, parseInput };
