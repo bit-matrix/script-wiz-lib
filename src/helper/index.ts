6981011b4a59237c515edb30d1e7986c620cb833
Umut ISIK <uuisik@gmail.com>
2021-04-05 23:11:49 +0300

    getNumberByteLength added.

--- src/helper/index.ts
+++ src/helper/index.ts
@@ -189,4 +189,55 @@ const parseInputData = (input: string): StackData => {
 //   return { input, value, display };
 // };
 
+/*
+Byte length for number x;
+
+-2147483648 < x < 2147483648
+n byte
+-1*2^(8n-1) < x <= -1*2^(8(n-1)-1) || 2^(8(n-1)-1) <= x < 2^(8n-1)
+
+1 byte:
+n = 1;
+-1*2^(8-1) < x <= -1*2^(0-1) || 2(0-1) <= x < 2^(8-1)
+-1*2^7 < x <= -1*2^(-1) || 2^(-1) <= x < 2'7
+-1*128 < x <= -1*(1/2) || 1/2 <= x < 128
+-128 < x <= -1/2 || 1/2 <= x < 128
+
+2 byte:
+n = 2;
+-1*2^(16-1) < x <= -1*2^(8-1) || 2^(8-1) <= x < 2^(16-1)
+-1*2^15 < x <= -1*2^7 || 2^7 <= x < 2^15
+-1*32768 < x <= -1*128 || 128 <= x < 32768
+-32768 < x <= -128 || 128 <= x < 32768
+
+3 byte:
+n = 3;
+-1*2^(24-1) < x <= -1*2^(16-1) || 2^(16-1) <= x < 2^(24-1)
+-1*2^23 < x <= -1*2^15 || 2^15 <= x < 2^23
+-1*8388608 < x <= -1*32768 || 32768 <= x < 8388608
+-8388608 < x <= -32768 || 32768 <= x < 8388608
+
+4 byte:
+n = 4;
+-1*2^(32-1) < x <= -1*2^(24-1) || 2^(24-1) <= x < 2^(32-1)
+-1*2^31 < x <= -1*2^23 || 2^23 <= x < 2^31
+-1*2147483648 < x <= -1*8388608 || 8388608 <= x < 2147483648
+-2147483648 < x <= -8388608 || 8388608 <= x < 2147483648
+*/
+
+const getNumberByteLength = (x: number) => {
+  let byteLength = 0;
+  if (x === 0) return byteLength;
+  const px = Math.abs(x);
+  for (let n = 1; n < 5; n++) {
+    const a = Math.pow(2, 8 * (n - 1) - 1);
+    const b = Math.pow(2, 8 * n - 1);
+    if ((-1 * b < x && x <= -1 * a) || (a <= x && x < b)) {
+      byteLength = n;
+      break;
+    }
+  }
+  return byteLength;
+};
+
 export { fillStackDataByte, fillStackDataNumber, fillStackDataString, hexLittleEndian, hexNumber, hexString, parseInput };
