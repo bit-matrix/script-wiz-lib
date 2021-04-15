e058580c0e4fdc73c9d38b54b0f20160c3052d28
Umut ISIK <uuisik@gmail.com>
2021-04-16 00:43:37 +0300

    run opcodes by number value

--- src/parse.ts
+++ src/parse.ts
@@ -1,3 +1,4 @@
+import { opcodeToWord } from "./helper";
 import stackHex from "./helper/stackHex";
 import stackNumber from "./helper/stackNumber";
 import OP from "./helper/stackOp";
@@ -10,9 +11,9 @@ const parseFinalInput = (input: string): StackData => {
   // 12
   // OP_...
 
+  // HEX DATA INPUT
   if (input.startsWith("0x")) {
     console.log("byte data input");
-    // byte data
     return stackHex(input);
   }
   // else if (input.match(EMOJI_REGEX)) {
@@ -24,12 +25,21 @@ const parseFinalInput = (input: string): StackData => {
   //     byteValue: "",
   //   };
   // }
+  // STRING INPUT
   else if ((input.startsWith('"') && input.endsWith('"')) || (input.startsWith("'") && input.endsWith("'"))) {
     // string data
 
     const formattedInput = input.substr(1, input.length - 2);
     return stackString(formattedInput);
-  } else if (!isNaN(input as any)) {
+  }
+  // OP_DATA INPUT
+  else if (input.startsWith("OP_")) {
+    //
+    // get number value of OP_ code from const array.
+    //
+  }
+  // NUMBER INPUT
+  if (!isNaN(input as any)) {
     console.log("number data input");
     // number
     return stackNumber(input);
@@ -42,17 +52,33 @@ const parseFinalInput = (input: string): StackData => {
 };
 
 const parse = (input: string, stackDataArray: StackData[]): StackDataResult => {
+  //
   if (input.startsWith("<") && input.endsWith(">")) {
     const finalInput = input.substr(1, input.length - 2);
     const data = parseFinalInput(finalInput);
     return { data, removeLastSize: 0 };
-  } else if (input === "OP_0" || input === "OP_FALSE") {
+  }
+
+  //
+  else if (input === "OP_0" || input === "OP_FALSE") {
     return { data: { byteValue: "0x00", input: "0x00", numberValue: 0, byteValueDisplay: "0" }, removeLastSize: 0 };
-  } else if (input.startsWith("OP_")) {
+  }
+  //
+  else if (!isNaN(input as any)) {
+  }
+  // OP Word or OP Code
+  if (input.startsWith("OP_") || !isNaN(input as any)) {
+    let word = input;
+    // Op Code
+    if (!isNaN(input as any)) {
+      word = opcodeToWord(Number(input));
+      if (word === "") throw "Unknown OP code number";
+    }
+
     const sLength = stackDataArray.length;
     if (sLength < 2) throw "Empty stack error";
     const thirth: StackData | undefined = sLength === 2 ? undefined : stackDataArray[sLength - 3];
-    return OP(input, stackDataArray[sLength - 1], stackDataArray[sLength - 2], thirth);
+    return OP(word, stackDataArray[sLength - 1], stackDataArray[sLength - 2], thirth);
   } else {
     throw "it is not a valid input or OP code";
   }
