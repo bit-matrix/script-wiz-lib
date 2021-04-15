4cc7f8f4444329b9610c55855d4ec7363f5a4c25
Umut ISIK <uuisik@gmail.com>
2021-04-16 00:42:10 +0300

    opWordCodes consts

---
+++ src/constant/opWordCodes.ts
@@ -0,0 +1,8 @@
+const opWordCodes = [
+  { word: "OP_CAT", opcode: 126 },
+  { word: "OP_SUBSTR", opcode: 127 },
+  { word: "OP_ADD", opcode: 147 },
+  { word: "OP_SUB", opcode: 148 },
+];
+
+export default opWordCodes;
