4b0f7f6d9fc6cab8dc5e5cd20a0c96ba709eeeb1
Umut ISIK <uuisik@gmail.com>
2021-04-16 02:10:58 +0300

    opWordCodes extend to hex

--- src/constant/opWordCodes.ts
+++ src/constant/opWordCodes.ts
@@ -1,11 +1,11 @@
 const opWordCodes = [
-  { word: "OP_0", opcode: 0 },
-  { word: "OP_FALSE", opcode: 0 },
+  { word: "OP_0", opcode: 0, hex: "0x00" },
+  { word: "OP_FALSE", opcode: 0, hex: "0x00" },
 
-  { word: "OP_CAT", opcode: 126 },
-  { word: "OP_SUBSTR", opcode: 127 },
-  { word: "OP_ADD", opcode: 147 },
-  { word: "OP_SUB", opcode: 148 },
+  { word: "OP_CAT", opcode: 126, hex: "0x7e" },
+  { word: "OP_SUBSTR", opcode: 127, hex: "0x7f" },
+  { word: "OP_ADD", opcode: 147, hex: "0x93" },
+  { word: "OP_SUB", opcode: 148, hex: "0x94" },
 ];
 
 export default opWordCodes;
