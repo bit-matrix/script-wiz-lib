9a675e0db0d0fcdddce4795f5b7c79b3eff8db42
Umut ISIK <uuisik@gmail.com>
2021-04-16 01:25:41 +0300

    OP_0, OP_FALSE and throws messages

--- src/constant/opWordCodes.ts
+++ src/constant/opWordCodes.ts
@@ -1,4 +1,7 @@
 const opWordCodes = [
+  { word: "OP_0", opcode: 0 },
+  { word: "OP_FALSE", opcode: 0 },
+
   { word: "OP_CAT", opcode: 126 },
   { word: "OP_SUBSTR", opcode: 127 },
   { word: "OP_ADD", opcode: 147 },
--- src/helper/stackOp.ts
+++ src/helper/stackOp.ts
@@ -1,4 +1,4 @@
-import { StackDataResult } from "../model";
+import { StackData, StackDataResult } from "../model";
 import IStackData from "../model/IStackData";
 import stackHex from "./stackHex";
 import stackNumber from "./stackNumber";
@@ -8,55 +8,72 @@ const OP_ADD = (stackData1: IStackData, stackData2: IStackData): IStackData => {
   if (stackData1.numberValue !== undefined && stackData2.numberValue !== undefined) {
     const totalValue: number = stackData1.numberValue + stackData2.numberValue;
     return stackNumber(totalValue.toString());
-  } else {
-    console.error("Invalid input: this operation requires a valid Script Number.");
-    throw "Invalid input: this operation requires a valid Script Number.";
   }
+
+  throw "OP_ADD Error: this operation requires 2 valid number data";
 };
 
 const OP_SUB = (stackData2: IStackData, stackData1: IStackData): IStackData => {
   if (stackData1.numberValue !== undefined && stackData2.numberValue !== undefined) {
     const totalValue: number = stackData2.numberValue - stackData1.numberValue;
     return stackNumber(totalValue.toString());
-  } else {
-    console.error("Invalid input: this operation requires a valid Script Number.");
-    throw "Invalid input: this operation requires a valid Script Number.";
   }
+
+  throw "OP_SUB Error: this operation requires 2 valid number data";
 };
 
 const OP_CAT = (stackData2: IStackData, stackData1: IStackData): IStackData => {
-  const byteValue = "0x" + stackData2.byteValue.substring(2) + stackData1.byteValue.substring(2);
-  return stackHex(byteValue);
+  const byteValue = "0x";
+  let firstByte1 = stackData2.byteValue.substring(2);
+  let firstByte2 = stackData1.byteValue.substring(2);
+
+  firstByte1 = firstByte1 === "00" ? "" : firstByte1;
+  firstByte2 = firstByte2 === "00" ? "" : firstByte2;
+
+  return stackHex(byteValue + firstByte1 + firstByte2);
 };
 
 // stackData 2 index  , stackData 1 size
 const OP_SUBSTR = (stackData3: IStackData, stackData2: IStackData, stackData1: IStackData): IStackData => {
   if (stackData3.stringValue !== undefined) {
-    if (stackData2.numberValue && stackData1.numberValue) {
+    if (stackData2.numberValue !== undefined && stackData1.numberValue !== undefined) {
       const resultString: string = stackData3.stringValue.substr(stackData2.numberValue, stackData1.numberValue);
       return stackString(resultString);
-    } else {
-      console.error("Index and size must be number !");
-      throw "Index and size must be number !";
     }
-  } else {
-    console.error("Data string value undefined !");
-    throw "Data string value undefined !";
+
+    throw "OP_SUBSTR Error: Index and size must be number!";
   }
+
+  throw "OP_SUBSTR Error: Invalid string value for sub string!";
 };
 
-const OP = (opCode: string, stackData1: IStackData, stackData2: IStackData, stackData3?: IStackData): StackDataResult => {
-  if (opCode === "OP_ADD") {
-    return { data: OP_ADD(stackData2, stackData1), removeLastSize: 2 };
-  } else if (opCode === "OP_SUB") {
-    return { data: OP_SUB(stackData2, stackData1), removeLastSize: 2 };
-  } else if (opCode === "OP_CAT") {
-    return { data: OP_CAT(stackData2, stackData1), removeLastSize: 2 };
-  } else if (opCode === "OP_SUBSTR" && stackData3) {
-    return { data: OP_SUBSTR(stackData3, stackData2, stackData1), removeLastSize: 3 };
-  } else {
-    throw "Invalid OP code!";
+const OP = (word: string, stackDataArray: StackData[]): StackDataResult => {
+  if (word === "OP_0" || word === "OP_FALSE") {
+    return { data: { byteValue: "0x00", input: "0x00", numberValue: 0, byteValueDisplay: "0" }, removeLastSize: 0 };
   }
+  const stackDataArrayLength = stackDataArray.length;
+
+  if (word === "OP_ADD") {
+    if (stackDataArrayLength < 2) throw "OP_ADD Error: stack data array must include min 2 data!";
+    return { data: OP_ADD(stackDataArray[stackDataArrayLength - 2], stackDataArray[stackDataArrayLength - 1]), removeLastSize: 2 };
+  }
+
+  if (word === "OP_SUB") {
+    if (stackDataArrayLength < 2) throw "OP_SUB Error: stack data array must include min 2 data!";
+    return { data: OP_SUB(stackDataArray[stackDataArrayLength - 2], stackDataArray[stackDataArrayLength - 1]), removeLastSize: 2 };
+  }
+
+  if (word === "OP_CAT") {
+    if (stackDataArrayLength < 2) throw "OP_CAT Error: stack data array must include min 2 data!";
+    return { data: OP_CAT(stackDataArray[stackDataArrayLength - 2], stackDataArray[stackDataArrayLength - 1]), removeLastSize: 2 };
+  }
+
+  if (word === "OP_SUBSTR") {
+    if (stackDataArrayLength < 3) throw "OP_SUBSTR Error: stack data array must include min 3 data!";
+    return { data: OP_SUBSTR(stackDataArray[stackDataArrayLength - 3], stackDataArray[stackDataArrayLength - 2], stackDataArray[stackDataArrayLength - 1]), removeLastSize: 3 };
+  }
+
+  throw "Unknown OP word!";
 };
 
 export default OP;
