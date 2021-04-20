07e77546d1c6f1a8c339698ded75ff176d6f3844
afarukcali <afarukcali@gmail.com>
2021-04-21 01:39:39 +0300

    dist

--- dist/helper/crypto.d.ts
+++ dist/helper/crypto.d.ts
@@ -2,4 +2,5 @@ declare const ripemd160: (data: string) => string;
 declare const sha1: (data: string) => string;
 declare const sha256: (data: string) => string;
 declare const hash160: (data: string) => string;
-export { ripemd160, sha1, sha256, hash160 };
+declare const hash256: (data: string) => string;
+export { hash160, hash256, ripemd160, sha1, sha256 };
