b0be8304461ff536a81f284dbb5e91438080526d
Umut ISIK <uuisik@gmail.com>
2021-04-20 23:20:09 +0300

    dist

--- dist/constant/opWordCodes.d.ts
+++ dist/constant/opWordCodes.d.ts
@@ -1,6 +1,9 @@
-declare const opWordCodes: {
+interface IOpWordCode {
     word: string;
     opcode: number;
     hex: string;
-}[];
+    output?: number;
+}
+declare const opWordCodes: IOpWordCode[];
 export default opWordCodes;
+export { IOpWordCode };
--- dist/helper/index.d.ts
+++ dist/helper/index.d.ts
@@ -1,5 +1,7 @@
+import { IOpWordCode } from "../constant/opWordCodes";
 declare const hexLittleEndian: (hex: string) => string;
+declare const opcodeToData: (word: string) => IOpWordCode | undefined;
 declare const opcodeToWord: (opcode: number) => string;
 declare const opWordToCode: (word: string) => number;
 declare const opWordToHex: (word: string) => string;
-export { hexLittleEndian, opcodeToWord, opWordToCode, opWordToHex };
+export { hexLittleEndian, opcodeToWord, opcodeToData, opWordToCode, opWordToHex };
