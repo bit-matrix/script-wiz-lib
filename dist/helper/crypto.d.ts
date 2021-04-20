4084ffb142cd4b2a0506f0784d4b63a25f81ee54
afarukcali <afarukcali@gmail.com>
2021-04-21 01:18:34 +0300

    dist

---
+++ dist/helper/crypto.d.ts
@@ -0,0 +1,4 @@
+declare const ripemd160: (data: string) => string;
+declare const sha1: (data: string) => string;
+declare const sha256: (data: string) => string;
+export { ripemd160, sha1, sha256 };
--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
+import { ripemd160, sha1, sha256 } from "./helper/crypto";
 import { StackData } from "./model";
 declare let stackDataArray: StackData[];
 declare const parse: (input: string) => StackData[];
 declare const clearStack: () => void;
-export { parse, clearStack, stackDataArray };
+export { parse, clearStack, stackDataArray, sha256, ripemd160, sha1 };
