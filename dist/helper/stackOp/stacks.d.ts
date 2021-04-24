7339d2522ad9f64e11c17af269015d8391a4660e
Umut ISIK <uuisik@gmail.com>
2021-04-25 00:07:44 +0300

    dist

--- dist/helper/stackOp/stacks.d.ts
+++ dist/helper/stackOp/stacks.d.ts
@@ -4,9 +4,10 @@ declare const OP_2DUP: (stackData1: IStackData, stackData2: IStackData) => IStac
 declare const OP_3DUP: (stackData1: IStackData, stackData2: IStackData, stackData3: IStackData) => IStackData[];
 declare const OP_2OVER: (stackData1: IStackData, stackData2: IStackData) => IStackData[];
 declare const OP_2SWAP: (stackData1: IStackData, stackData2: IStackData, stackData3: IStackData, stackData4: IStackData) => IStackData[];
+declare const OP_DEPTH: (stackDataArrayLenght: number) => IStackData[];
 declare const OP_DROP: () => IStackData[];
 declare const OP_DUP: (stackData1: IStackData) => IStackData[];
 declare const OP_NIP: (stackData1: IStackData, stackData2: IStackData) => IStackData[];
 declare const OP_OVER: (stackData: IStackData) => IStackData[];
 declare const OP_SWAP: (stackData1: IStackData, stackData2: IStackData) => IStackData[];
-export { OP_2DROP, OP_2DUP, OP_3DUP, OP_2OVER, OP_2SWAP, OP_DROP, OP_DUP, OP_NIP, OP_OVER, OP_SWAP };
+export { OP_2DROP, OP_2DUP, OP_3DUP, OP_2OVER, OP_2SWAP, OP_DEPTH, OP_DROP, OP_DUP, OP_NIP, OP_OVER, OP_SWAP };
