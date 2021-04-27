3ae09457c98465e2949f33b77e7211e2d74dfe8a
Umut ISIK <uuisik@gmail.com>
2021-04-28 01:09:06 +0300

    dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,5 +1,6 @@
 import { StackDataList } from "./model";
+import * as uu from "./uu/uu";
 declare let stackDataList: StackDataList;
 declare const parse: (input: string) => StackDataList;
 declare const clearStack: () => void;
-export { parse, clearStack, stackDataList };
+export { parse, clearStack, stackDataList, uu };
---
+++ dist/uu/uu.d.ts
@@ -0,0 +1,9 @@
+export declare const resizeUint8Array: (uint8Array: Uint8Array, byteLength: number) => Uint8Array;
+export declare const numeralNextValue: (value: number, base: number) => {
+    numeral: number;
+    nextValue: number;
+};
+export declare const toUint8Array: (value: number) => Uint8Array;
+export declare const toUint8ArrayResizedUint16: (value: number) => Uint8Array;
+export declare const toUint8ArrayResizedUint32: (value: number) => Uint8Array;
+export declare const toUint8ArrayResizedUint64: (value: number) => Uint8Array;
