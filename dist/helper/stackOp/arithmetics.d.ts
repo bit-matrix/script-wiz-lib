ff238dcd6f976b4dd70fb35122f5360797003f4e
afarukcali <afarukcali@gmail.com>
2021-05-01 14:19:43 +0300

    dist

--- dist/helper/stackOp/arithmetics.d.ts
+++ dist/helper/stackOp/arithmetics.d.ts
@@ -1,6 +1,8 @@
 import IStackData from "../../model/IStackData";
 declare const OP_ADD: (stackData1: IStackData, stackData2: IStackData) => IStackData[];
 declare const OP_SUB: (stackData2: IStackData, stackData1: IStackData) => IStackData[];
+declare const OP_MUL: (stackData1: IStackData, stackData2: IStackData) => IStackData[];
+declare const OP_DIV: (stackData1: IStackData, stackData2: IStackData) => IStackData[];
 declare const OP_LSHIFT: (stackData2: IStackData, stackData1: IStackData) => IStackData[];
 declare const OP_RSHIFT: (stackData2: IStackData, stackData1: IStackData) => IStackData[];
-export { OP_ADD, OP_SUB, OP_LSHIFT, OP_RSHIFT };
+export { OP_ADD, OP_SUB, OP_MUL, OP_DIV, OP_LSHIFT, OP_RSHIFT };
