2e2dd3a3146c4829119b519844614cec2e74bd84
Umut ISIK <uuisik@gmail.com>
2021-05-04 23:12:21 +0300

    dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,5 @@
 import { StackDataList } from "./model";
-import * as uu from "./uu/uu";
 declare let stackDataList: StackDataList;
 declare const parse: (input: string) => StackDataList;
 declare const clearStack: () => void;
-export { parse, clearStack, stackDataList, uu };
+export { parse, clearStack, stackDataList };
