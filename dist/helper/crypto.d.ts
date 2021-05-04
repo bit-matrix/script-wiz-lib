d4d2223bc1f34466f80a47bbef85e28c98c4129d
afarukcali <afarukcali@gmail.com>
2021-05-04 03:03:07 +0300

    dist

--- dist/helper/crypto.d.ts
+++ dist/helper/crypto.d.ts
@@ -3,4 +3,5 @@ declare const sha1: (data: string) => string;
 declare const sha256: (data: string) => string;
 declare const hash160: (data: string) => string;
 declare const hash256: (data: string) => string;
-export { hash160, hash256, ripemd160, sha1, sha256 };
+declare const ecdsaVerify: (signature: string, message: string, publicKey: string) => boolean;
+export { hash160, hash256, ripemd160, sha1, sha256, ecdsaVerify };
--- dist/helper/stackOp/cryptos.d.ts
+++ dist/helper/stackOp/cryptos.d.ts
@@ -4,4 +4,5 @@ declare const OP_SHA1: (stackData: IStackData) => IStackData[];
 declare const OP_SHA256: (stackData: IStackData) => IStackData[];
 declare const OP_HASH160: (stackData: IStackData) => IStackData[];
 declare const OP_HASH256: (stackData: IStackData) => IStackData[];
-export { OP_RIPEMD160, OP_SHA1, OP_SHA256, OP_HASH160, OP_HASH256 };
+declare const OP_CHECKSIGFROMSTACK: (stackData1: IStackData, stackData2: IStackData, stackData3: IStackData) => IStackData[];
+export { OP_RIPEMD160, OP_SHA1, OP_SHA256, OP_HASH160, OP_HASH256, OP_CHECKSIGFROMSTACK };
