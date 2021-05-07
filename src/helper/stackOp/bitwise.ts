8e89edfc510771e78afff304617b3ecbc9145764
Umut ISIK <uuisik@gmail.com>
2021-05-07 22:20:30 +0300

    OP_AND, OP_OR negative number fix

--- src/helper/stackOp/bitwise.ts
+++ src/helper/stackOp/bitwise.ts
@@ -1,21 +1,37 @@
 import { StackData } from "../../model";
 import stackNumber from "../stackNumber";
 import { checkByteValuesEquality } from "../index";
+import stackHex from "../stackHex";
 
 const OP_INVERT = (stackData: StackData): StackData[] => {
   return [stackNumber((~Number(stackData.byteValue)).toString())];
 };
 
 const OP_AND = (stackData1: StackData, stackData2: StackData): StackData[] => {
-  if (stackData1.byteValue.length !== stackData2.byteValue.length) throw "Script attempted a bitwise operation on operands of different lengths.";
-  const logic = Number(stackData1.byteValue) & Number(stackData2.byteValue);
-  return [stackNumber(logic.toString())];
+  if (stackData1.byteValue.length !== stackData2.byteValue.length) throw "OP_AND Error: Script attempted a bitwise operation on operands of different lengths.";
+  const number1 = stackHex(stackData1.byteValue).numberValue;
+  const number2 = stackHex(stackData2.byteValue).numberValue;
+  if (number1 && number2) {
+    const isNegative = number1 < 0 && number2 < 0;
+    let logic = Math.abs(number1) & Math.abs(number2);
+    logic = isNegative ? logic * -1 : logic;
+    return [stackNumber(logic.toString())];
+  }
+  throw "OP_AND Error: Counld't convert to number";
 };
 
 const OP_OR = (stackData1: StackData, stackData2: StackData): StackData[] => {
-  if (stackData1.byteValue.length !== stackData2.byteValue.length) throw "Script attempted a bitwise operation on operands of different lengths.";
-  const logic = Number(stackData1.byteValue) | Number(stackData2.byteValue);
-  return [stackNumber(logic.toString())];
+  if (stackData1.byteValue.length !== stackData2.byteValue.length) throw "OP_OR Error: Script attempted a bitwise operation on operands of different lengths.";
+  const number1 = stackHex(stackData1.byteValue).numberValue;
+  const number2 = stackHex(stackData2.byteValue).numberValue;
+  if (number1 && number2) {
+    const isNegative = number1 < 0 || number2 < 0;
+    let logic = Math.abs(number1) | Math.abs(number2);
+    logic = isNegative ? logic * -1 : logic;
+    return [stackNumber(logic.toString())];
+  }
+
+  throw "OP_OR Error: Counld't convert to number";
 };
 
 const OP_XOR = (stackData1: StackData, stackData2: StackData): StackData[] => {
