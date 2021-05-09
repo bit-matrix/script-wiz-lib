0f18934beb7918d8cd796d4dc53ece94b88d79c9
afarukcali <afarukcali@gmail.com>
2021-05-09 14:46:38 +0300

    OP_XOR review.

--- src/helper/stackOp/bitwise.ts
+++ src/helper/stackOp/bitwise.ts
@@ -31,14 +31,30 @@ const OP_OR = (stackData1: StackData, stackData2: StackData): StackData[] => {
     logic = isNegative ? logic * -1 : logic;
     return [stackNumber(logic.toString())];
   }
-
   throw "OP_OR Error: Counld't convert to number";
 };
 
 const OP_XOR = (stackData1: StackData, stackData2: StackData): StackData[] => {
-  if (stackData1.byteValue.length !== stackData2.byteValue.length) throw "Script attempted a bitwise operation on operands of different lengths.";
-  const logic = Number(stackData1.byteValue) ^ Number(stackData2.byteValue);
-  return [stackNumber(logic.toString())];
+  if (stackData1.byteValue.length !== stackData2.byteValue.length) throw "OP_XOR Error: Script attempted a bitwise operation on operands of different lengths.";
+  const number1 = stackData1.numberValue;
+  const number2 = stackData2.numberValue;
+
+  if (number1 !== undefined && number2 !== undefined) {
+    const isNegative = number1 * number2 < 0;
+
+    let logic = Math.abs(number1) ^ Math.abs(number2);
+    logic = isNegative ? logic * -1 : logic;
+
+    let logicByteValue = stackNumber(logic.toString()).byteValue;
+
+    //higher than byte
+    if (stackData1.byteValue.length > 4 && stackData1.byteValue.length !== logicByteValue.length) {
+      logicByteValue = logicByteValue + "00";
+    }
+
+    return [stackHex(logicByteValue)];
+  }
+  throw "OP_XOR Error: Counld't convert to number";
 };
 
 const OP_EQUAL = (stackData1: StackData, stackData2: StackData): StackData[] => {
@@ -54,3 +70,6 @@ const OP_EQUAL = (stackData1: StackData, stackData2: StackData): StackData[] => 
 const OP_EQUALVERIFY = (stackData1: StackData, stackData2: StackData): boolean => checkByteValuesEquality(stackData1.byteValue, stackData2.byteValue);
 
 export { OP_INVERT, OP_AND, OP_OR, OP_XOR, OP_EQUAL, OP_EQUALVERIFY };
+function hexNumber(logic: number) {
+  throw new Error("Function not implemented.");
+}
