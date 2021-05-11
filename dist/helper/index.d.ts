843ea3b7e5d0dcd3cfccfd09f20b188039eeb685
afarukcali <afarukcali@gmail.com>
2021-05-11 15:52:53 +0300

    dist

--- dist/helper/index.d.ts
+++ dist/helper/index.d.ts
@@ -6,6 +6,5 @@ declare const opcodeToWord: (opcode: number) => string;
 declare const opWordToCode: (word: string) => number;
 declare const opWordToHex: (word: string) => string;
 declare const currentScope: (stackDataList: StackDataList) => boolean;
-declare const checkByteValuesEquality: (byte1: string, byte2: string) => boolean;
 declare const invertBits: (n: number) => number;
-export { hexLittleEndian, invertBits, opcodeToWord, opcodeToData, opWordToCode, opWordToHex, currentScope, checkByteValuesEquality, };
+export { hexLittleEndian, invertBits, opcodeToWord, opcodeToData, opWordToCode, opWordToHex, currentScope };
