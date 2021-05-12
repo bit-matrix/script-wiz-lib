6c776edd238b1a344d2c721420a0750348d0d2fe
afarukcali <afarukcali@gmail.com>
2021-05-12 13:23:55 +0300

    dist

--- dist/helper/stackOp/splices.d.ts
+++ dist/helper/stackOp/splices.d.ts
@@ -1,5 +1,6 @@
 import IStackData from "../../model/IStackData";
 declare const OP_CAT: (stackData2: IStackData, stackData1: IStackData) => IStackData[];
 declare const OP_SUBSTR: (stackData3: IStackData, stackData2: IStackData, stackData1: IStackData) => IStackData[];
+declare const OP_RIGHT: (stackData2: IStackData, stackData1: IStackData) => IStackData[];
 declare const OP_SIZE: (stackData: IStackData) => IStackData[];
-export { OP_CAT, OP_SUBSTR, OP_SIZE };
+export { OP_CAT, OP_SUBSTR, OP_RIGHT, OP_SIZE };
