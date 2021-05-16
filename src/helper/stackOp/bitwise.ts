ffbcfc1d0ff5bbadddc413e78f60cfdde93c601c
Umut ISIK <uuisik@gmail.com>
2021-05-17 00:02:01 +0300

    OP_XOR by bytes

--- src/helper/stackOp/bitwise.ts
+++ src/helper/stackOp/bitwise.ts
@@ -34,27 +34,52 @@ const OP_OR = (stackData1: StackData, stackData2: StackData): StackData[] => {
 };
 
 const OP_XOR = (stackData1: StackData, stackData2: StackData): StackData[] => {
-  if (stackData1.byteValue.length !== stackData2.byteValue.length) throw "OP_XOR Error: Script attempted a bitwise operation on operands of different lengths.";
-  const number1 = stackData1.numberValue;
-  const number2 = stackData2.numberValue;
+  const x = stackData1.byteValue;
+  const y = stackData2.byteValue;
+  if (x.length !== y.length) throw "OP_XOR Error: Script attempted a bitwise operation on operands of different lengths.";
 
-  if (number1 !== undefined && number2 !== undefined) {
-    const isNegative = number1 * number2 < 0;
+  let resultHex = "0x";
 
-    let logic = Math.abs(number1) ^ Math.abs(number2);
-    logic = isNegative ? logic * -1 : logic;
-
-    let logicByteValue = stackNumber(logic.toString()).byteValue;
-
-    //higher than byte
-    if (stackData1.byteValue.length > 4 && stackData1.byteValue.length !== logicByteValue.length) {
-      logicByteValue = logicByteValue + "00";
-    }
-
-    return [stackHex(logicByteValue)];
+  const xHexArray = x
+    .match(/.{1,2}/g)
+    ?.slice(1)
+    .map((m) => m.toString());
+  const yHexArray = y
+    .match(/.{1,2}/g)
+    ?.slice(1)
+    .map((m) => m.toString());
+  if (xHexArray && yHexArray) {
+    const xorUInt8Array = xHexArray.map((xHex, i) => parseInt(xHex, 16) ^ parseInt(yHexArray[i], 16));
+    const xorHexArray = xorUInt8Array.map((xorUInt8) => xorUInt8.toString(16).padStart(2, "0"));
+    resultHex += xorHexArray.join("");
   }
-  throw "OP_XOR Error: Counld't convert to number";
+  resultHex = resultHex.padEnd(x.length, "0");
+
+  return [stackHex(resultHex)];
 };
+
+// const OP_XOR = (stackData1: StackData, stackData2: StackData): StackData[] => {
+//   if (stackData1.byteValue.length !== stackData2.byteValue.length) throw "OP_XOR Error: Script attempted a bitwise operation on operands of different lengths.";
+//   const number1 = stackData1.numberValue;
+//   const number2 = stackData2.numberValue;
+
+//   if (number1 !== undefined && number2 !== undefined) {
+//     const isNegative = number1 * number2 < 0;
+
+//     let logic = Math.abs(number1) ^ Math.abs(number2);
+//     logic = isNegative ? logic * -1 : logic;
+
+//     let logicByteValue = stackNumber(logic.toString()).byteValue;
+
+//     //higher than byte
+//     if (stackData1.byteValue.length > 4 && stackData1.byteValue.length !== logicByteValue.length) {
+//       logicByteValue = logicByteValue + "00";
+//     }
+
+//     return [stackHex(logicByteValue)];
+//   }
+//   throw "OP_XOR Error: Counld't convert to number";
+// };
 
 const OP_EQUAL = (stackData1: StackData, stackData2: StackData): StackData[] => {
   const expression = stackData1.byteValue === stackData2.byteValue;
