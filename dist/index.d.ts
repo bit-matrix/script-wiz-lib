5d21d500af63bb4b855ad7f88ad390657b514292
Umut ISIK <uuisik@gmail.com>
2021-05-18 02:44:55 +0300

    dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,5 +5,6 @@ import compileFinalInput from "./compileFinalInput";
 import { compileData, compileJoin } from "./compileAll";
 declare let stackDataList: StackDataList;
 declare const parse: (input: string) => StackDataList;
+declare const compileScript: () => string;
 declare const clearStack: () => void;
-export { compileFinalInput, parse, parseFinalInput, clearStack, opWordCodes, stackDataList, compileData, compileJoin };
+export { compileFinalInput, parse, parseFinalInput, clearStack, opWordCodes, stackDataList, compileData, compileJoin, compileScript };
