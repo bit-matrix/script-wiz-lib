8a8716fea77787b2eb5f6a5c3bb7f779f0ebafb0
Umut ISIK <uuisik@gmail.com>
2021-05-21 01:03:17 +0300

    convertion number

--- src/convertion/number.ts
+++ src/convertion/number.ts
@@ -2,6 +2,15 @@ const BIT_8 = 8;
 const BIT_16 = 16;
 const BIT_32 = 32;
 
+const log = (base: number, x: number) => Math.log(x) / Math.log(base);
+
+const numberByteLength = (x: number) => {
+  if (x === 0) return 0;
+  else if (0 < x) return Math.ceil((log(2, x + 1) + 1) / 8);
+  else if (x < 0) return Math.floor((log(2, -x) + 1) / 8 + 1);
+  return 0;
+};
+
 const resizeBytes = (uint8Array: Uint8Array, byteLength: number): Uint8Array => {
   const resizedUint8Array = new Uint8Array(byteLength);
   if (uint8Array.length > byteLength) {
@@ -19,7 +28,7 @@ const numeralNextValue = (value: number, base: number): { numeral: number; nextV
   return { numeral, nextValue };
 };
 
-export const numberToBytes = (value: number): Uint8Array => {
+const uint8NumberToBytes = (value: number): Uint8Array => {
   const baseNumber = Math.pow(2, BIT_8);
   let lastValue = value;
   const numeralArray: number[] = [];
@@ -35,7 +44,14 @@ export const numberToBytes = (value: number): Uint8Array => {
   return result;
 };
 
-export const numberToBytesResizedUint16 = (value: number): Uint8Array => {
+export const numberToBytes = (value: number): Uint8Array => {
+  const byteLength = numberByteLength(value);
+  const inputNumber = value < 0 ? Math.pow(2, 8 * byteLength - 1) - value : value;
+  const uint8NumberBytes = uint8NumberToBytes(inputNumber);
+  return resizeBytes(uint8NumberBytes, byteLength);
+};
+
+/* export const numberToBytesResizedUint16 = (value: number): Uint8Array => {
   const uint8Array = numberToBytes(value);
   const resizedUint8Array = resizeBytes(uint8Array, 2);
   return resizedUint8Array;
@@ -51,7 +67,7 @@ export const numberToBytesResizedUint64 = (value: number): Uint8Array => {
   const uint8Array = numberToBytes(value);
   const resizedUint8Array = resizeBytes(uint8Array, 8);
   return resizedUint8Array;
-};
+}; */
 
 /* export const toUint16Array = (value: number) => {
   const baseNumber = Math.pow(2, BIT_16);
