ff76bd39ab7c6ec11fb563c05ffe27a8aaa9f3e3
Umut ISIK <uuisik@gmail.com>
2021-05-20 14:57:30 +0300

    string utfs bytes convertion

---
+++ src/convertion/bytes.ts
@@ -0,0 +1,56 @@
+const validByte = (byte: number) => 0 <= byte || byte <= 255;
+
+const bytesLE = (bytes: Uint8Array): Uint8Array => bytes.reverse();
+
+const byteToHex = (byte: number): string => {
+  if (!validByte(byte)) throw "byteToHex: invalid byte number";
+  return byte.toString(16).padStart(2, "0");
+};
+
+const bytesToHex = (bytes: Uint8Array): string => bytes.reduce((hexString, currentByte) => hexString + byteToHex(currentByte), "");
+
+/**
+ * This implementations is derived from:
+ * https://github.com/google/closure-library/blob/8598d87242af59aac233270742c8984e2b2bdbe0/closure/goog/crypt/crypt.js
+ *
+ * Copyright 2008 The Closure Library Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS-IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+const bytesToString = (bytes: Uint8Array) => {
+  var out = [],
+    pos = 0,
+    c = 0;
+  while (pos < bytes.length) {
+    var c1 = bytes[pos++];
+    if (c1 < 128) {
+      out[c++] = String.fromCharCode(c1);
+    } else if (c1 > 191 && c1 < 224) {
+      var c2 = bytes[pos++];
+      out[c++] = String.fromCharCode(((c1 & 31) << 6) | (c2 & 63));
+    } else if (c1 > 239 && c1 < 365) {
+      // Surrogate Pair
+      var c2 = bytes[pos++];
+      var c3 = bytes[pos++];
+      var c4 = bytes[pos++];
+      var u = (((c1 & 7) << 18) | ((c2 & 63) << 12) | ((c3 & 63) << 6) | (c4 & 63)) - 0x10000;
+      out[c++] = String.fromCharCode(0xd800 + (u >> 10));
+      out[c++] = String.fromCharCode(0xdc00 + (u & 1023));
+    } else {
+      var c2 = bytes[pos++];
+      var c3 = bytes[pos++];
+      out[c++] = String.fromCharCode(((c1 & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+    }
+  }
+  return out.join("");
+};
--- src/convertion/hex.ts
+++ src/convertion/hex.ts
@@ -1,5 +1,7 @@
 const validHex = (hex: string) => hex.length % 2 === 0 && !/[^a-fA-F0-9]/u.test(hex);
 
+const hexLE = (hex: string): string => bytesToHex(hexToBytes(hex).reverse());
+
 const hexToHexBytes = (hex: string): string[] => {
   if (!validHex(hex)) throw "hexToHexBytes: invalid hex string";
   const matches: RegExpMatchArray | null = hex.match(/.{1,2}/g);
@@ -14,4 +16,4 @@ const hexToByte = (hex: string): number => {
 
 const hexToBytes = (hex: string): Uint8Array => Uint8Array.from(hexToHexBytes(hex).map((byte) => hexToByte(byte)));
 
-const hexLE = (hex: string): string => bytesToHex(hexToBytes(hex).reverse());
+const hexToString = (hex: string): string => bytesToString(hexToBytes(hex));
