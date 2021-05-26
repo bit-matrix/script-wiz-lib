47ef54b1972a99f838b8279f7dd88da185c0b187
Penguen Umut <uuisik@gmail.com>
2021-05-27 01:20:27 +0300

    hexBoundaries and comments

--- src/convertion/hex.ts
+++ src/convertion/hex.ts
@@ -1,8 +1,8 @@
-import { bytesToHex, bytesToString } from "./bytes";
+// import { bytesToHex, bytesToString } from "./bytes";
 
 const validHex = (hex: string) => hex.length % 2 === 0 && !/[^a-fA-F0-9]/u.test(hex);
 
-const hexLE = (hex: string): string => bytesToHex(hexToBytes(hex).reverse());
+// const hexLE = (hex: string): string => bytesToHex(hexToBytes(hex).reverse());
 
 const hexToHexBytes = (hex: string): string[] => {
   if (!validHex(hex)) throw "hexToHexBytes: invalid hex string";
@@ -18,4 +18,30 @@ const hexToByte = (hex: string): number => {
 
 export const hexToBytes = (hex: string): Uint8Array => Uint8Array.from(hexToHexBytes(hex).map((byte) => hexToByte(byte)));
 
-const hexToString = (hex: string): string => bytesToString(hexToBytes(hex));
+// const hexToString = (hex: string): string => bytesToString(hexToBytes(hex));
+
+interface NumberBoundries {
+  minPos: number;
+  maxPos: number;
+  minNeg: number;
+  maxNeg: number;
+}
+
+export const hexBoundaries = (bytesLength: number): NumberBoundries | undefined => {
+  // 2^(8n-9) <= x <= 2^(8n-1) - 1
+  // -2(8n-9) >= x >= -2^(8n-1) + 1
+
+  const b1 = Math.pow(2, 8 * bytesLength - 9);
+  const b2 = Math.pow(2, 8 * bytesLength - 1);
+
+  if (0 < bytesLength && bytesLength < 5) {
+    return {
+      minPos: b1,
+      maxPos: b2 - 1,
+      minNeg: 1 - b2,
+      maxNeg: -1 * b1,
+    };
+  }
+
+  return;
+};
