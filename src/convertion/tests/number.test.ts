0f759d0fa173a02874da9cce8f4a30d17be017c0
Penguen Umut <uuisik@gmail.com>
2021-06-02 00:27:33 +0300

    test number: hex to data.bin values

--- src/convertion/tests/number.test.ts
+++ src/convertion/tests/number.test.ts
@@ -30,6 +30,10 @@ test("Data class: hex to data object test", () => {
     const data: Data = Data.fromHex(d.hexValue.substring(2));
 
     expect(data.bytes.length).toBe(d.byteLength);
+    expect(data.bin.length / 8).toBe(d.byteLength);
+    expect(data.hex.length / 2).toBe(d.byteLength);
+
+    expect(data.bin).toBe(d.binValue.substring(2));
     expect(data.hex).toBe(d.hexValue.substring(2));
 
     if (-MAX_INTEGER <= d.inputNumber && d.inputNumber <= MAX_INTEGER) {
