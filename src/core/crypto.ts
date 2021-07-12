e507a10f14e14cfee24d37b7a160cc5ba5770c48
afarukcali <afarukcali@gmail.com>
2021-07-12 18:52:36 +0300

    crypto : crypto functions added

--- src/core/crypto.ts
+++ src/core/crypto.ts
@@ -1,82 +1,87 @@
 import CryptoJS from "crypto-js";
 import elliptic from "elliptic";
 import BN from "bn.js";
+import WizData from "../convertion";
 
-const RIPEMD160 = (data: string) => {
-  return CryptoJS.RIPEMD160(CryptoJS.enc.Hex.parse(data.substr(2))).toString();
+export const ripemd160 = (wizData: WizData): CryptoJS.lib.WordArray => {
+  return CryptoJS.RIPEMD160(wizData.hex);
 };
 
-const SHA1 = (data: string) => {
-  return CryptoJS.SHA1(CryptoJS.enc.Hex.parse(data.substr(2))).toString();
+export const sha1 = (wizData: WizData): CryptoJS.lib.WordArray => {
+  return CryptoJS.SHA1(wizData.hex);
 };
 
-const SHA256 = (data: string) => {
-  return CryptoJS.SHA256(CryptoJS.enc.Hex.parse(data.substr(2))).toString();
+export const sha256 = (wizData: WizData): CryptoJS.lib.WordArray => {
+  return CryptoJS.SHA256(wizData.hex);
 };
 
-const HASH160 = (data: string) => {
-  const dataWithSha256Hashed = CryptoJS.SHA256(CryptoJS.enc.Hex.parse(data.substr(2)));
-  const dataWithRipemd160Hashed = CryptoJS.RIPEMD160(dataWithSha256Hashed).toString();
-
+export const hash160 = (wizData: WizData): CryptoJS.lib.WordArray => {
+  const dataWithSha256Hashed = CryptoJS.SHA256(wizData.hex);
+  const dataWithRipemd160Hashed = CryptoJS.RIPEMD160(dataWithSha256Hashed);
   return dataWithRipemd160Hashed;
 };
 
-const HASH256 = (data: string) => {
-  const firstSHAHash = CryptoJS.SHA256(CryptoJS.enc.Hex.parse(data.substr(2)));
-  const secondSHAHash = CryptoJS.SHA256(firstSHAHash).toString();
+export const hash256 = (wizData: WizData): CryptoJS.lib.WordArray => {
+  const firstSHAHash = CryptoJS.SHA256(wizData.hex);
+  const secondSHAHash = CryptoJS.SHA256(firstSHAHash);
 
   return secondSHAHash;
 };
 
-const ECDSA = (messageHash: string, publicKey: string): string => {
-  const EC = elliptic.ec;
-
-  // Create and initialize EC context
-  // (better do it once and reuse it)
-  const ec = new EC("secp256k1");
-
-  // Generate keys
-  const key = ec.genKeyPair();
-
-  // Sign the message's hash (input must be an array, or a hex-string)
-  const signature = key.sign(messageHash);
-
-  // Export DER encoded signature in Array
-  const derSign = signature.toDER();
-
-  return derSign;
-};
-
-const ECDSAVerify = (signature: string, message: string, publicKey: string): boolean => {
+export const ecdsaVerify = (signature: WizData, message: WizData, publicKey: WizData): boolean => {
   const secp256k1 = new elliptic.ec("secp256k1");
-  const hashedMessage = SHA256(message);
 
-  if (publicKey.length !== 68) throw "ECDSA Verify error : invalid public key length";
+  const hashedMessage = sha256(message);
 
-  if (!signature.startsWith("0x30")) throw "ECDSA Verify error : signature must start with 0x30";
+  const publicKeyHex = publicKey.hex;
+  const signatrueHex = signature.hex;
 
-  const rAndSDataSize = Number("0x" + signature.substr(4, 2));
+  if (publicKeyHex.length !== 66) throw "ECDSA Verify error : invalid public key length";
 
-  const signatureStringLength = rAndSDataSize * 2 + 6;
+  if (!signatrueHex.startsWith("30")) throw "ECDSA Verify error : signature must start with 0x30";
 
-  if (signature.length !== signatureStringLength) throw "ECDSA Verify error : signature length invalid";
+  const rAndSDataSize = Number("0x" + signatrueHex.substr(2, 2));
 
-  const rDataSize = Number("0x" + signature.substr(8, 2));
+  const signatureStringLength = rAndSDataSize * 2 + 4;
 
-  const rValue = signature.substr(10, rDataSize * 2);
+  if (signatrueHex.length !== signatureStringLength) throw "ECDSA Verify error : signature length invalid";
 
-  const sDataSize = Number("0x" + signature.substr(12 + rDataSize * 2, 2));
+  const rDataSize = Number("0x" + signatrueHex.substr(6, 2));
 
-  const sValue = signature.substr(12 + rDataSize * 2 + 2, sDataSize * 2);
+  const rValue = signatrueHex.substr(10, rDataSize * 2);
+
+  const sDataSize = Number("0x" + signatrueHex.substr(10 + rDataSize * 2, 2));
+
+  const sValue = signatrueHex.substr(12 + rDataSize * 2 + 2, sDataSize * 2);
+
+  console.log(rValue);
+  console.log(sValue);
 
   const rBn = new BN(rValue, "hex");
   const sBn = new BN(sValue, "hex");
 
   try {
-    return secp256k1.verify(hashedMessage, { r: rBn, s: sBn }, secp256k1.keyFromPublic(publicKey.slice(2), "hex"));
+    return secp256k1.verify(hashedMessage.toString(), { r: rBn, s: sBn }, secp256k1.keyFromPublic(publicKey.hex, "hex"));
   } catch {
     throw "ECDSA Verify error : something went wrong";
   }
 };
 
-export { HASH160, HASH256, RIPEMD160, SHA1, SHA256, ECDSA, ECDSAVerify };
+// const ECDSA = (messageHash: string, publicKey: string): string => {
+//   const EC = elliptic.ec;
+
+//   // Create and initialize EC context
+//   // (better do it once and reuse it)
+//   const ec = new EC("secp256k1");
+
+//   // Generate keys
+//   const key = ec.genKeyPair();
+
+//   // Sign the message's hash (input must be an array, or a hex-string)
+//   const signature = key.sign(messageHash);
+
+//   // Export DER encoded signature in Array
+//   const derSign = signature.toDER();
+
+//   return derSign;
+// };
