e9ae73a3d721ab8b4028e44fe8d50af3036f6361
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-07-13 15:36:00 +0300

    test: splices test functions added.

--- src/core/tests/splices.test.ts
+++ src/core/tests/splices.test.ts
@@ -1,6 +1,13 @@
 // import { numberTestData } from "./data/number";
 import WizData from "../../convertion";
-import { concatenate, substr } from "../splices";
+import {
+  concatenate,
+  left,
+  right,
+  size,
+  substr,
+  substr_lazy,
+} from "../splices";
 
 test("Splices concatenate test", () => {
   const wizData: WizData = WizData.fromHex("0001");
@@ -25,4 +32,40 @@ test("Splices substr test", () => {
   const result: WizData = substr(message, index, size);
 
   expect(result.hex).toBe("637269");
+});
+
+test("Splices right test", () => {
+  const message: WizData = WizData.fromText("scriptwiz");
+  const size: WizData = WizData.fromNumber(3);
+
+  const result: WizData = right(message, size);
+
+  expect(result.hex).toBe("77697a");
+});
+
+test("Splices left test", () => {
+  const message: WizData = WizData.fromText("scriptwiz");
+  const size: WizData = WizData.fromNumber(3);
+
+  const result: WizData = left(message, size);
+
+  expect(result.hex).toBe("736372");
+});
+
+test("Splices size test", () => {
+  const message: WizData = WizData.fromText("scriptwiz");
+
+  const result: WizData = size(message);
+
+  expect(result.number).toBe(9);
+});
+
+test("Splices substr_lazy test", () => {
+  const message: WizData = WizData.fromText("scriptwiz");
+  const index: WizData = WizData.fromNumber(2);
+  const size: WizData = WizData.fromNumber(3);
+
+  const result: WizData = substr_lazy(message, index, size);
+
+  expect(result.hex).toBe("726970");
 });
