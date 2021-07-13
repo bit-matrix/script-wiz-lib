ed597d4024333d99d492483001c3efb656fa847c
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-07-13 15:27:21 +0300

    helper.ts: getRandomNumber min value converted to negative

--- src/core/tests/helper.ts
+++ src/core/tests/helper.ts
@@ -1,7 +1,7 @@
 import { MAX_INTEGER } from "../../convertion";
 
 export const getRandomNumber = (min?: number, max?: number): number => {
-  let minValue = min || 0;
+  let minValue = min || -MAX_INTEGER;
   let maxValue = max || MAX_INTEGER;
 
   return Math.random() * (maxValue - minValue) + minValue;
