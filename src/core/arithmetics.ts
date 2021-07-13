b4b831c6229e6bfcd0bf6a812ef6ffe9d6dc2095
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-07-13 15:03:42 +0300

    arithmetics: div fix

--- src/core/arithmetics.ts
+++ src/core/arithmetics.ts
@@ -87,7 +87,9 @@ export const div = (wizData: WizData, wizData2: WizData): WizData => {
   if (wizData.number !== undefined && wizData2.number !== undefined) {
     if (wizData2.number === 0) throw "Error: dividing can't be eqaul 0.";
 
-    const divValue: number = wizData.number / wizData2.number;
+    let divValue: number = wizData.number / wizData2.number;
+
+    divValue = divValue > 0 ? Math.floor(divValue) : Math.ceil(divValue);
     return WizData.fromNumber(divValue);
   }
 
@@ -114,7 +116,8 @@ export const rshift = (wizData: WizData, wizData2: WizData): WizData => {
 
 export const boolAnd = (wizData: WizData, wizData2: WizData): WizData => {
   if (wizData.number !== undefined && wizData2.number !== undefined) {
-    if (wizData.number === 0 || wizData2.number === 0) return WizData.fromNumber(0);
+    if (wizData.number === 0 || wizData2.number === 0)
+      return WizData.fromNumber(0);
     return WizData.fromNumber(1);
   }
 
@@ -123,7 +126,8 @@ export const boolAnd = (wizData: WizData, wizData2: WizData): WizData => {
 
 export const boolOr = (wizData: WizData, wizData2: WizData): WizData => {
   if (wizData.number !== undefined && wizData2.number !== undefined) {
-    if (wizData.number === 0 && wizData2.number === 0) return WizData.fromNumber(0);
+    if (wizData.number === 0 && wizData2.number === 0)
+      return WizData.fromNumber(0);
     return WizData.fromNumber(1);
   }
 
@@ -138,7 +142,10 @@ export const numEqual = (wizData: WizData, wizData2: WizData): WizData => {
   throw "Error: this operation requires 2 valid number wizData";
 };
 
-export const numEqualVerify = (wizData: WizData, wizData2: WizData): WizData => {
+export const numEqualVerify = (
+  wizData: WizData,
+  wizData2: WizData
+): WizData => {
   return numEqual(wizData, wizData2);
 };
 
@@ -163,7 +170,10 @@ export const graterThan = (wizData: WizData, wizData2: WizData): WizData => {
   throw "Error: this operation requires 2 valid number wizData";
 };
 
-export const lessThanOrEqual = (wizData: WizData, wizData2: WizData): WizData => {
+export const lessThanOrEqual = (
+  wizData: WizData,
+  wizData2: WizData
+): WizData => {
   if (wizData.number !== undefined && wizData2.number !== undefined) {
     return WizData.fromNumber(wizData.number <= wizData2.number ? 1 : 0);
   }
@@ -171,7 +181,10 @@ export const lessThanOrEqual = (wizData: WizData, wizData2: WizData): WizData =>
   throw "Error: this operation requires 2 valid number wizData";
 };
 
-export const graterThanOrEqual = (wizData: WizData, wizData2: WizData): WizData => {
+export const graterThanOrEqual = (
+  wizData: WizData,
+  wizData2: WizData
+): WizData => {
   if (wizData.number !== undefined && wizData2.number !== undefined) {
     return WizData.fromNumber(wizData.number >= wizData2.number ? 1 : 0);
   }
@@ -199,13 +212,22 @@ export const max = (wizData: WizData, wizData2: WizData): WizData => {
   throw "Error: this operation requires 2 valid number wizData";
 };
 
-export const withIn = (wizData: WizData, wizData2: WizData, wizData3: WizData): WizData => {
+export const withIn = (
+  wizData: WizData,
+  wizData2: WizData,
+  wizData3: WizData
+): WizData => {
   const currentNumber = wizData.number;
   const minValue = wizData2.number;
   const maxValue = wizData3.number;
 
-  if (currentNumber !== undefined && minValue !== undefined && maxValue !== undefined) {
-    if (currentNumber >= minValue && currentNumber <= maxValue) return WizData.fromNumber(1);
+  if (
+    currentNumber !== undefined &&
+    minValue !== undefined &&
+    maxValue !== undefined
+  ) {
+    if (currentNumber >= minValue && currentNumber <= maxValue)
+      return WizData.fromNumber(1);
 
     return WizData.fromNumber(0);
   }
