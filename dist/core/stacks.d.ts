c9d1c2302a2567a527c114e08d49e0ae00028396
afarukcali <afarukcali@gmail.com>
2021-07-29 14:52:07 +0300

    dist

--- dist/core/stacks.d.ts
+++ dist/core/stacks.d.ts
@@ -1,7 +1,5 @@
 import WizData from "../convertion";
-export declare const toAltStack: () => void;
 export declare const fromAltStack: (wizData: WizData) => WizData;
-export declare const twoDrop: () => void;
 export declare const twoDup: (wizData: WizData, wizData2: WizData) => WizData[];
 export declare const threeDup: (wizData: WizData, wizData2: WizData, wizData3: WizData) => WizData[];
 export declare const twoOver: (wizData: WizData, wizData2: WizData) => WizData[];
@@ -9,7 +7,6 @@ export declare const rot: (wizData: WizData, wizData2: WizData, wizData3: WizDat
 export declare const twoRot: (wizData: WizData, wizData2: WizData, wizData3: WizData, wizData4: WizData, wizData5: WizData, wizData6: WizData) => WizData[];
 export declare const twoSwap: (wizData: WizData, wizData2: WizData, wizData3: WizData, wizData4: WizData) => WizData[];
 export declare const depth: (length: number) => WizData;
-export declare const drop: () => never[];
 export declare const dup: (wizData: WizData) => WizData;
 export declare const ifDup: (wizData: WizData) => WizData | {};
 export declare const nip: (wizData: WizData) => WizData;
