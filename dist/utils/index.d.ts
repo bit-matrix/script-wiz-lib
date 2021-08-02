d6c822aa6994af9122f99fe5258a243e116ecb2c
afarukcali <afarukcali@gmail.com>
2021-08-02 17:02:45 +0300

    dist

--- dist/utils/index.d.ts
+++ dist/utils/index.d.ts
@@ -3,5 +3,6 @@ import { Opcode } from "../opcodes/model/Opcode";
 export declare const flipbits: (str: string) => string;
 export declare const opHexToWord: (hex: string, opWordCodes: Opcode[]) => string;
 export declare const opcodeToWord: (opcode: number, opWordCodes: Opcode[]) => string;
+export declare const opWordToHex: (word: string, opWordCodes: Opcode[]) => string;
 export declare const currentScope: (wizDataList: WizDataList) => boolean;
 export declare const EMOJI_REGEX: RegExp;
