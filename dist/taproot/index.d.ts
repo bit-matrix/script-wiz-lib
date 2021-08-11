85d0973ad1d6237bfbb87ffd2efb876904a34d33
afarukcali <afarukcali@gmail.com>
2021-08-11 15:57:29 +0300

    dist

--- dist/taproot/index.d.ts
+++ dist/taproot/index.d.ts
@@ -1,7 +1,7 @@
 export declare const tagHash: (tag: string, data: Uint8Array) => string;
-export declare const treeHelper: (script: string) => {
+export declare const treeHelper: (script: string, version: string) => {
     data: string;
     h: string;
 };
-export declare const tapRoot: (pubKey: string, script: string) => void;
-export declare const tweakAdd: () => void;
+export declare const tweakAdd: (pubkey: Uint8Array, tweak: Uint8Array) => string;
+export declare const tapRoot: (pubKey: string, script: string, version?: string) => string;
