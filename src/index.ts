7952c5d3e44d8f83911121649b32645995148dab
afarukcali <afarukcali@gmail.com>
2021-08-11 17:10:12 +0300

    taproot : taproot export

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { WizDataList } from "./model";
 import { VM, VM_NETWORK, VM_NETWORK_VERSION } from "./opcodes/model/VM";
 import { ScriptWiz } from "./scriptWiz";
 import { compileFinalInput } from "./scriptWiz/compileFinalInput";
+import { tapRoot } from "./taproot";
 
 // TO-DO compilefinalinput will remove
-export { compileFinalInput, ScriptWiz, WizData, WizDataList, VM, VM_NETWORK, VM_NETWORK_VERSION };
+export { compileFinalInput, ScriptWiz, WizData, WizDataList, VM, VM_NETWORK, VM_NETWORK_VERSION, tapRoot };
