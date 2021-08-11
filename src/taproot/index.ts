ff758e9ddb876411419109600881c9901c48deca
afarukcali <afarukcali@gmail.com>
2021-08-11 15:09:35 +0300

    taproot : tweak add function added.

--- src/taproot/index.ts
+++ src/taproot/index.ts
@@ -1,6 +1,7 @@
 import WizData from "../convertion";
 import { sha256 } from "../core/crypto";
 import { toHexString } from "../utils";
+import * as secp256k1 from "secp256k1";
 
 export const tagHash = (tag: string, data: Uint8Array) => {
   let hashedTag = sha256(WizData.fromText(tag)).toString();
@@ -21,4 +22,14 @@ export const treeHelper = (script: string) => {
   const h = tagHash("TapLeaf", WizData.fromHex(scriptData).bytes);
 
   return { data: versionData + scriptLength + script, h };
+};
+
+export const tapRoot = (pubKey: string, script: string) => {
+  const { h } = treeHelper(script);
+  const tweak = tagHash("TapTweak", WizData.fromHex(pubKey + h).bytes);
+};
+
+export const tweakAdd = (pubkey: Uint8Array, tweak: Uint8Array): string => {
+  const tweaked = secp256k1.publicKeyTweakAdd(pubkey, tweak);
+  return toHexString(tweaked);
 };
--- src/taproot/taproot.test.ts
+++ src/taproot/taproot.test.ts
@@ -1,4 +1,5 @@
-import { tagHash, treeHelper } from ".";
+import { tagHash, treeHelper, tweakAdd } from ".";
+import WizData from "../convertion";
 
 test("taghash test", () => {
   let byteArrayData = new Uint8Array(1);
@@ -6,9 +7,12 @@ test("taghash test", () => {
 
   const tag = "TapLeaf";
 
-  const result = tagHash(tag, byteArrayData);
+  const result = tagHash(
+    "TapTweak",
+    WizData.fromHex("03e3c4cda9de3c372bba88dc68d680adf3beee6c3067deedfb9991758ad0bcdeaf" + "0f20d41260bd81c46f4ee8a388b0f139d107f707e38fb2525f191b83a49c5013").bytes
+  );
 
-  expect(result).toBe("51ea46053abf55dbe44701ee1738b6ed35c8a0d3c4496142710ff45ac7c8cb16");
+  expect(result).toBe("9aacaf264e5801ce3fb094dedd7435977f6ebb9e5a0d99a70406525158cf9244");
 });
 
 test("tree helper test", () => {
@@ -17,4 +21,16 @@ test("tree helper test", () => {
 
   expect(result.data).toBe("c00455935787");
   expect(result.h).toBe("0f20d41260bd81c46f4ee8a388b0f139d107f707e38fb2525f191b83a49c5013");
+
+  WizData.fromHex("021dae61a4a8f841952be3a511502d4f56e889ffa0685aa0098773ea2d4309f624").bytes,
+    WizData.fromHex("542d433b81daf3e28069bccbb0d951d7d9ca57cc0f563f2de126e91deba7d6a6").bytes;
+});
+
+test("tweakAdd test", () => {
+  const pubkey = WizData.fromHex("021dae61a4a8f841952be3a511502d4f56e889ffa0685aa0098773ea2d4309f624").bytes;
+  const tweak = WizData.fromHex("542d433b81daf3e28069bccbb0d951d7d9ca57cc0f563f2de126e91deba7d6a6").bytes;
+
+  const result = tweakAdd(pubkey, tweak);
+
+  expect(result).toBe("0326fef75b96729c1753eeac93309ae90c8a06192ea5b1b13175e239743ec11c4a");
 });
