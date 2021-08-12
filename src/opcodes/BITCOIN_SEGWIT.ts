65959b200ee4a51c64240cce1dbfd2b707263469
afarukcali <afarukcali@gmail.com>
2021-08-12 22:56:53 +0300

    opcodes : bitcoin opcodes fixed.

--- src/opcodes/BITCOIN_SEGWIT.ts
+++ src/opcodes/BITCOIN_SEGWIT.ts
@@ -1,4 +1,7 @@
 import { Opcode } from "./model/Opcode";
 import { commonOpcodes } from "./common";
 
-export const opcodesBitcoinSegwit: Opcode[] = [...commonOpcodes];
+export const opcodesBitcoinSegwit: Opcode[] = [
+  ...commonOpcodes,
+  { word: "OP_EQUAL", opcode: 135, hex: "0x87", description: "Returns 1 if the inputs are exactly equal, 0 otherwise." },
+]; //	x1 x2	True / false	Returns 1 if the inputs are exactly equal, 0 otherwise.];
