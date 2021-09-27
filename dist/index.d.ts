e13cd24d25dd482af10315e0c04a19129477a90c
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-09-27 19:53:09 +0300

    dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,7 @@
+import { hash160v2, sha256v2 } from "./core/crypto";
 import { WizDataList } from "./model";
 import { VM, VM_NETWORK, VM_NETWORK_VERSION } from "./opcodes/model/VM";
 import { ScriptWiz } from "./scriptWiz";
 import { compileFinalInput } from "./scriptWiz/compileFinalInput";
 import { tapRoot } from "./taproot";
-export { compileFinalInput, ScriptWiz, WizDataList, VM, VM_NETWORK, VM_NETWORK_VERSION, tapRoot };
+export { compileFinalInput, ScriptWiz, WizDataList, VM, VM_NETWORK, VM_NETWORK_VERSION, tapRoot, sha256v2, hash160v2 };
