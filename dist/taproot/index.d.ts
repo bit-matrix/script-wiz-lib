17088b7a5cca28ba8c7b8d772e1eb9c6d2ac253d
hüseyin sarıtaş <huseyinsaritas@test-MacBook-Pro.local>
2021-10-15 16:58:46 +0300

    dist

--- dist/taproot/index.d.ts
+++ dist/taproot/index.d.ts
@@ -2,8 +2,5 @@ import WizData from "@script-wiz/wiz-data";
 import { Taproot } from "./model";
 export declare const tweakAdd: (pubkey: Uint8Array, tweak: Uint8Array) => WizData;
 export declare const tagHash: (tag: string, data: WizData) => string;
-export declare const treeHelper: (script: WizData, version: string) => {
-    data: string;
-    h: string;
-};
-export declare const tapRoot: (pubKey: WizData, script: WizData, version?: string) => Taproot;
+export declare const treeHelper: (scripts: WizData[], version: string) => string;
+export declare const tapRoot: (pubKey: WizData, scripts: WizData[], version?: string) => Taproot;
