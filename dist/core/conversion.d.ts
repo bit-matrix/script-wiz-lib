3906e1861ec0d06d0e89b243752f9ad2edd834fb
afarukcali <afarukcali@gmail.com>
2021-11-10 19:13:35 +0300

    dist

--- dist/core/conversion.d.ts
+++ dist/core/conversion.d.ts
@@ -1,3 +1,4 @@
 import WizData from "@script-wiz/wiz-data";
 export declare const numToLE64: (wizData: WizData) => WizData;
 export declare const LE64ToNum: (wizData: WizData) => WizData;
+export declare const LE32toLE64: (wizData: WizData) => WizData;
