260264c587ff69e4470e38cc685241ae0234419b
afarukcali <afarukcali@gmail.com>
2021-11-19 22:55:14 +0300

    dist

---
+++ dist/core/arithmetics64.d.ts
@@ -0,0 +1,10 @@
+import WizData from "@script-wiz/wiz-data";
+export declare const add64: (wizData: WizData, wizData2: WizData) => WizData;
+export declare const sub64: (wizData: WizData, wizData2: WizData) => WizData;
+export declare const mul64: (wizData: WizData, wizData2: WizData) => WizData;
+export declare const div64: (wizData: WizData, wizData2: WizData) => WizData;
+export declare const neg64: (wizData: WizData) => WizData;
+export declare const lessThan64: (wizData: WizData, wizData2: WizData) => WizData;
+export declare const lessThanOrEqual64: (wizData: WizData, wizData2: WizData) => WizData;
+export declare const greaterThan64: (wizData: WizData, wizData2: WizData) => WizData;
+export declare const greaterThanOrEqual64: (wizData: WizData, wizData2: WizData) => WizData;
