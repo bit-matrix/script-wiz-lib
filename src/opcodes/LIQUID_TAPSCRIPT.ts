df81ccc46bf3befc25fb2e250ed63ebc251fa8bb
afarukcali <afarukcali@gmail.com>
2021-11-20 02:54:22 +0300

    Liquid tapscript op codes extend fixed.

--- src/opcodes/LIQUID_TAPSCRIPT.ts
+++ src/opcodes/LIQUID_TAPSCRIPT.ts
@@ -1,9 +1,7 @@
 import { Opcode } from "./model/Opcode";
-import { opcodesBitcoinTapscript } from "./BITCOIN_TAPSCRIPT";
 import { opcodesLiquidSegwit } from "./LIQUID_SEGWIT";
 
 export const opcodesLiquidTapscript: Opcode[] = [
-  ...opcodesBitcoinTapscript,
   ...opcodesLiquidSegwit,
 
   /*
