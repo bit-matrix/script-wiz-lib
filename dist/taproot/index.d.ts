ff594b7030a8e29ea3fbda0cba912be7d811914e
afarukcali <afarukcali@gmail.com>
2021-11-29 01:45:24 +0300

    dist

--- dist/taproot/index.d.ts
+++ dist/taproot/index.d.ts
@@ -1,6 +1,7 @@
 import WizData from "@script-wiz/wiz-data";
 import { Taproot } from "./model";
 export declare const tweakAdd: (pubkey: WizData, tweak: WizData) => WizData;
+export declare const publicKeyTweakCheck: (pubkey: WizData, tweak: WizData, expect: WizData, negate: boolean) => boolean;
 export declare const tagHash: (tag: string, data: WizData) => string;
 export declare const treeHelper: (scripts: WizData[], version: string) => string;
 export declare const tapRoot: (pubKey: WizData, scripts: WizData[], version?: string) => Taproot;
