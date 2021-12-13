d827ff088b972aa86182befff4c9deefb3b06c89
afarukcali <afarukcali@gmail.com>
2021-12-13 23:48:03 +0300

    dist

--- dist/model/WizDataList.d.ts
+++ dist/model/WizDataList.d.ts
@@ -1,5 +1,5 @@
 import WizData from "@script-wiz/wiz-data";
-import { TxData } from "./TxData";
+import { model } from "@script-wiz/lib-core";
 export declare type WizDataList = {
     inputHexes: string[];
     main: WizData[];
@@ -8,5 +8,5 @@ export declare type WizDataList = {
     altFlow: boolean[];
     isStackFailed: boolean;
     errorMessage?: string;
-    txData?: TxData;
+    txData?: model.TxData;
 };
---
+++ dist/scriptWiz/flow.d.ts
@@ -0,0 +1,19 @@
+import WizData from "@script-wiz/wiz-data";
+import { WizDataList } from "../model";
+export declare const flowVerify: (wizData: WizData) => boolean;
+export declare const flowIf: (wizDataList: WizDataList) => {
+    flow: boolean[];
+    altFlow: boolean[];
+};
+export declare const flowNotIf: (wizDataList: WizDataList) => {
+    flow: boolean[];
+    altFlow: boolean[];
+};
+export declare const flowElse: (wizDataList: WizDataList) => {
+    flow: boolean[];
+    altFlow: boolean[];
+};
+export declare const flowEndIf: (wizDataList: WizDataList) => {
+    flow: boolean[];
+    altFlow: boolean[];
+};
