6a97244bf014e11f2a8864342b4a593a81801be0
afarukcali <afarukcali@gmail.com>
2021-12-13 23:55:08 +0300

    dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 import { WizDataList } from "./model";
 import { VM, VM_NETWORK, VM_NETWORK_VERSION } from "./opcodes/model/VM";
-import { ScriptWiz } from "./scriptWiz";
+import { ScriptWiz } from "./ScriptWiz";
 export { ScriptWiz, WizDataList, VM, VM_NETWORK, VM_NETWORK_VERSION };
---
+++ dist/parse.d.ts
@@ -0,0 +1,6 @@
+import WizData from "@script-wiz/wiz-data";
+import { ParseResult, WizDataList } from "./model";
+import { Opcode } from "./opcodes/model/Opcode";
+import { VM } from "./opcodes/model/VM";
+export declare const parse: (opWordCodes: Opcode[], stackDataList: WizDataList, currentScopeParse: boolean, currentScopeParseException: boolean, inputHexParam?: string | undefined, inputNumberParam?: number | undefined, inputTextParam?: string | undefined, inputBinParam?: string | undefined, inputOpCodeParam?: string | undefined, version?: VM | undefined) => ParseResult;
+export declare const parseValueInputs: (inputHexParam?: string | undefined, inputNumberParam?: number | undefined, inputTextParam?: string | undefined, inputBinParam?: string | undefined) => WizData;
