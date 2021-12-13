a17de40dd02fe4ef1bd9103780299de5b9dd0b78
afarukcali <afarukcali@gmail.com>
2021-12-13 16:39:12 +0300

    utils review

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,5 @@
 import { WizDataList } from "../model";
 import { Opcode } from "../opcodes/model/Opcode";
-import { BigInteger } from "big-integer";
-import bigInt from "big-integer";
 import WizData from "@script-wiz/wiz-data";
 import BN from "bn.js";
 import { convert64 } from "../core/conversion";
@@ -31,34 +29,6 @@ export const toHexString = (byteArray: Uint8Array) => {
   return Array.from(byteArray, function (byte) {
     return ("0" + (byte & 0xff).toString(16)).slice(-2);
   }).join("");
-};
-
-export const log2 = (a: BigInteger) => {
-  const s = a.toString(16);
-  const c = s.charCodeAt(0) - "0".charCodeAt(0);
-  if (c <= 0) {
-    throw new RangeError();
-  }
-  return (s.length - 1) * 4 + (31 - Math.clz32(Math.min(c, 8)));
-};
-
-// y^2 = x^3 + 7
-export const formattedPubkey = (pubkey: string): WizData => {
-  if (pubkey.length !== 64) throw "Pubkey length must be equal 32 byte";
-
-  const pubkeyBigInt: BigInteger = bigInt(pubkey, 16);
-
-  const pubkeyPow = pubkeyBigInt.pow(3);
-
-  const addedPubkey = pubkeyPow.add(bigInt(7));
-
-  const log2Result = log2(addedPubkey);
-
-  if (log2Result % 2 === 0) {
-    return WizData.fromHex("02" + pubkey);
-  }
-
-  return WizData.fromHex("03" + pubkey);
 };
 
 // supports all opcodes
