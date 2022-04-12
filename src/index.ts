e43277368a90403f27771f271cc2902a4ee351a7
afarukcali <afarukcali@gmail.com>
2022-04-12 23:14:18 +0300

    export compile data function.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { WizDataList } from "./model";
 import { VM, VM_NETWORK, VM_NETWORK_VERSION } from "./opcodes/model/VM";
 import { ScriptWiz } from "./ScriptWiz";
+import { compileData } from "./utils/compileAll";
 
-export { ScriptWiz, WizDataList, VM, VM_NETWORK, VM_NETWORK_VERSION };
+export { ScriptWiz, WizDataList, VM, VM_NETWORK, VM_NETWORK_VERSION, compileData };
