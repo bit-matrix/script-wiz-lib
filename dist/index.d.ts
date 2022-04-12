463f034b44dcec634fc0660d3e61278c5d2104b9
afarukcali <afarukcali@gmail.com>
2022-04-12 23:14:58 +0300

    dist.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,5 @@
 import { WizDataList } from "./model";
 import { VM, VM_NETWORK, VM_NETWORK_VERSION } from "./opcodes/model/VM";
 import { ScriptWiz } from "./ScriptWiz";
-export { ScriptWiz, WizDataList, VM, VM_NETWORK, VM_NETWORK_VERSION };
+import { compileData } from "./utils/compileAll";
+export { ScriptWiz, WizDataList, VM, VM_NETWORK, VM_NETWORK_VERSION, compileData };
--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.VM_NETWORK_VERSION = exports.VM_NETWORK = exports.ScriptWiz = void 0;
+exports.compileData = exports.VM_NETWORK_VERSION = exports.VM_NETWORK = exports.ScriptWiz = void 0;
 var VM_1 = require("./opcodes/model/VM");
 Object.defineProperty(exports, "VM_NETWORK", { enumerable: true, get: function () { return VM_1.VM_NETWORK; } });
 Object.defineProperty(exports, "VM_NETWORK_VERSION", { enumerable: true, get: function () { return VM_1.VM_NETWORK_VERSION; } });
 var ScriptWiz_1 = require("./ScriptWiz");
 Object.defineProperty(exports, "ScriptWiz", { enumerable: true, get: function () { return ScriptWiz_1.ScriptWiz; } });
+var compileAll_1 = require("./utils/compileAll");
+Object.defineProperty(exports, "compileData", { enumerable: true, get: function () { return compileAll_1.compileData; } });
 //# sourceMappingURL=index.js.map
